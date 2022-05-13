7073f0ad3ce020f7f634452fbec0d58924082e0c
newtonaltnetter <89472435+newtonaltnetter@users.noreply.github.com>
2022-05-13 15:44:35 -0300

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,3 +1,5 @@
+#https://github.com/newtonaltnetter
+
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -5,12 +7,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# we set the chromedriver path here
 driver = webdriver.Chrome(service=Service("path/to/chromedriver.exe"))
 
 
+# function to scrape video url
 def searchVideo(x):
     driver.get("https://www.youtube.com/results?search_query=" + x)
+    
+    # sleep for 2 seconds
     time.sleep(2)
+    
+    # here the driver takes the first video
     video = driver.find_element(By.XPATH, '//*[@id="video-title"]')
     videoEmb = video.get_attribute("href")
     titleVideo = video.get_attribute("title")
@@ -21,9 +29,11 @@ def searchVideo(x):
     return midias
 
 
-print("Aguarde, os dados estão sendo coletados...")
+print("Please wait, data is being collected...")
 
+# these are the words you want to search for
 args = ["house", "camping", "etc..", "etc.."]
 for x in args:
+    # call the function
     midia = searchVideo(x)
     print(midia)
